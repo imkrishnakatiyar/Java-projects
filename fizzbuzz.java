954fe1dd21d84ff12d8bd737bced50ebdc312d42
imkrishnakatiyar <90976299+imkrishnakatiyar@users.noreply.github.com>
2022-01-21 18:38:23 +0530

    Update fizzbuzz.java

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,7 +1,25 @@
 class fizzbuzz
 {
     public static void main(String args[]) {
-       
+      for(int i = 1 ; i<=100 ; i++){
+       if(i%15==0)
+       {
+         System.out.println("fizz buzz");
+       }
+       else if(i%3==0)
+       {
+         System.out.println("fizz");
+       }
+       else if(i%5==0)
+       {
+         System.out.println("buzz");
+       }
+       else{
+          System.out.println(i);
+       }
+      }
       } 
     }
+
+
 
